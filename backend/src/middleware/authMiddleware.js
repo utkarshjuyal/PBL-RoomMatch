3e9551a687ca5ba3c334c5f5fb472849d06ce02e
utkarshjuyal <utkarshjuyal8@gmail.com>
2026-09-02 22:52:55 +0530

    Phase 2: Step 21 complete

--- backend/src/middleware/authMiddleware.js
+++ backend/src/middleware/authMiddleware.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 const protect = (req, res, next) => {
     try {
         const authHeader = req.headers.authorization;
-
+        console.log("Authorization Header:", authHeader); 
         if (!authHeader) {
             return res.status(401).json({
                 message: "Authentication required"
