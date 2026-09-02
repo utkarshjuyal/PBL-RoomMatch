418b5b3ab566e610de6045a19d285268e1c362fb
utkarshjuyal <utkarshjuyal8@gmail.com>
2026-09-02 22:11:59 +0530

    Phase 2: Step 20 complete

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -6,25 +6,62 @@ const {
 } = require("../controllers/authController");
 
 const protect = require("../middleware/authMiddleware");
+const authorize = require("../middleware/roleMiddleware");
 
 const router = express.Router();
 
 
-// Public routes
+// ==============================
+// PUBLIC
+// ==============================
 
 router.post("/register", register);
 
 router.post("/login", login);
 
 
-// Protected route
+// ==============================
+// AUTHENTICATED
+// ==============================
 
 router.get("/me", protect, (req, res) => {
-    res.status(200).json({
-        message: "You are authenticated",
+    res.json({
+        message: "Authenticated user",
         user: req.user
     });
 });
+
+
+// ==============================
+// TENANT ONLY
+// ==============================
+
+router.get(
+    "/tenant-test",
+    protect,
+    authorize("tenant"),
+    (req, res) => {
+        res.json({
+            message: "Welcome Tenant!"
+        });
+    }
+);
+
+
+// ==============================
+// LANDLORD ONLY
+// ==============================
+
+router.get(
+    "/landlord-test",
+    protect,
+    authorize("landlord"),
+    (req, res) => {
+        res.json({
+            message: "Welcome Landlord!"
+        });
+    }
+);
 
 
 module.exports = router;
